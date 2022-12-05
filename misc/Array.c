0374d4f25bd655b06c81f4e39fa2c18c3ee032d2
brat_volk <43145630+brat-volk@users.noreply.github.com>
2022-12-05 16:09:25 +0100

    Add files via upload

---
+++ misc/Array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <windows.h>
+#define Q void
+#define o int
+??=defi??/
+ne _??/
+_ 30
+??=define q for(??/
+__[O]
+%:define _ +
+??=define FALSE 1
+#define $ putchar
+??=define _break ??/
+10
+#define _case $(__ _ 18 _ ______ %
+#define __break _break *
+
+o ____( ______ )o ______ ;<%switch(0);_case (__break _break) / _break);_case _break);$(__break 3 _ 2);}
+
+Q main(Q)
+<%o *O= (o[__ _ FALSE]){};//??/
+return (NULL & 0x48) = '-'-'-' /11;
+srand(GetTickCount());
+__asm__("xor %1,%0" : "=r"(*(O-1)) : "r"(*(O-1)));/**O^=*O;*/
+q; *(O _ __) < __ ; O??( __ ??)-=-1)
+*(FALSE ??' FALSE ? NULL : &O[ __ [O]??)) += rand()%26 _ 10;
+q ??'= O[ __ ]; *(O _ __) < __; O??(__??)++)
+____( *(O _ *(O _ __)));??>
