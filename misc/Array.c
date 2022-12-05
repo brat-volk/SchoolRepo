c945c00956a807340f048c26d74c44f22e3d7061
brat_volk <43145630+brat-volk@users.noreply.github.com>
2022-12-05 21:24:38 +0100

    Add files via upload

--- misc/Array.c
+++ misc/Array.c
@@ -2,27 +2,28 @@
 #include <windows.h>
 #define Q void
 #define o int
-??=defi??/
-ne _??/
-_ 30
-??=define q for(??/
+??=define o_ for(??/
 __[O]
-%:define _ +
-??=define FALSE 1
+%:define _ - -
+??=define _O 1
 #define $ putchar
 ??=define _break ??/
-10
+ ((1 << 3) + 2)
 #define _case $(__ _ 18 _ ______ %
 #define __break _break *
+??=defi??/
+ne _??/
+_ (__break 3)
 
-o ____( ______ )o ______ ;<%switch(0);_case (__break _break) / _break);_case _break);$(__break 3 _ 2);}
+o ____( ______ )o ______ ;<%switch(0);_case (__break _break) / _break);_case _break);$(__ + 2);}
 
 Q main(Q)
-<%o *O= (o[__ _ FALSE]){};//??/
+<%o *O= (o[__ _ _O]){};//??/
 return (NULL & 0x48) = '-'-'-' /11;
 srand(GetTickCount());
-__asm__("xor %1,%0" : "=r"(*(O-1)) : "r"(*(O-1)));/**O^=*O;*/
-q; *(O _ __) < __ ; O??( __ ??)-=-1)
-*(FALSE ??' FALSE ? NULL : &O[ __ [O]??)) += rand()%26 _ 10;
-q ??'= O[ __ ]; *(O _ __) < __; O??(__??)++)
+__asm__("xor %1,%0" : "=r"(*(O-_O)) : "r"(*(O-_O)));
+o_; *(O _ __) < __ ; O??( __ ??)-=-_O)
+*(_O ??' _O ? 0 : &O[ __ [O]??)) += rand()%(__ - \
+(_O << (_O << _O))) _ __ / (_O _ _O _ _O);
+o_ ??'= O[ __ ]; *(O _ __) < __; O??(__??)++)
 ____( *(O _ *(O _ __)));??>
