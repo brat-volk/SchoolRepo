369dbf21548c3173527eb8b2e9b6d17fa69a9ce5
brat_volk <43145630+brat-volk@users.noreply.github.com>
2022-12-09 18:36:15 +0100

    Delete 15_11_22 directory

--- 15_11_22/es1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void main(void){
-    int a;
-    const char* MSG[2]= {"Bisestile","Non Bisestile"};
-    printf("\nInserire anno: ");
-    scanf("%d",&a);
-    printf(MSG[((a%400==0 | a%100!=0) & a%4==0) ? 0 : 1]);
-}
