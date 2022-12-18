291fed2c9bdccfe581c99906b50a6c7aae7e9ca4
brat_volk <43145630+brat-volk@users.noreply.github.com>
2022-12-18 22:22:22 +0100

    Add files via upload

---
+++ 22_12_16/obf.c
@@ -0,0 +1,39 @@
+#include <immintrin.h>
+#include <setjmp.h>
+#define I ;
+#define oO printf
+#define o0 char
+#define OO +2
+#define o ((1<<3) OO)
+#define Oo oO(
+struct {o0* _;}typedef volatile oo;oo* O0[]={(o0[]){8,8,32,8,8,13,10,13,10,0},(o0[]){37,117,44,32,0}} I o0 _(void* ___)<%longjmp(___,1);??>;
+void main(void){
+    jmp_buf __;
+    register unsigned int* restrict O =  (int[(o*2)+3]){};
+    if(setjmp( __ ))
+        goto _continue;
+    for(*(O+(o*2) OO) = 0; *(O+(o*2) OO) < o; (o*2 OO)[O]-=('-'-'-'-1)){
+        sizeof(int[_rdrand32_step(O+*(O+(o*2) OO))]);
+        sizeof(int[_rdrand32_step(O+o+*(O+(o*2) OO))]);
+        //sizeof(int[ O[O[22]]%= 101]);sizeof(int[ O[o+O[22]]%= 101]);                                      //range toggle
+        sizeof(int[*(O+(o*2))=(*(O+(o*2)) < *(O+*(O+(o*2) OO)))?*(O+*(O+(o*2) OO)):*(O+(o*2))]);
+        sizeof(int[*(O+(o*2)+1)=(*(O+(o*2)+1)<*(O+o+*(O+(o*2) OO)))?*(O+o+*(O+(o*2 OO))):*(O+(o*2)+1)]);
+    }
+    *(O+(o*2) OO) = 0;
+    switch(0) while(1){
+    default:case 1:
+        *((o*2) OO + O)=*((o*2) OO + O) OO - 1;
+        sizeof(int[Oo *(O0+1),*(O+*(O+(o*2) OO)))]);
+        if( *(O+(o*2) OO) < o)
+            continue;
+        _(&__);
+    }
+    _continue:puts(*O0);
+    for(I I *((o*2) OO + O)=*((o*2) OO + O) OO - 1){
+        sizeof(int[Oo *(O0+1),*(O+*(O+(o*2) OO)))]);
+        if(*(O+(o*2) OO)==(o*2)-1)
+            break;
+    }
+    puts(*O0);
+    sizeof(int[Oo"%u, %u", *(O+(o*2)),*(O+(o*2)+1))]);
+}
